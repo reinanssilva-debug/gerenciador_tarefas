80649d002dfa94bff97d0c28a4377d5682fefaf4
Reinan_aluno <reinan.s.silva@aluno.senai.br>
2025-11-11 16:38:18 -0300

    arquivo javaScript criado

---
+++ script.js
@@ -0,0 +1,12 @@
+const apiUrl= "http://localhosot;3000/tasks"
+
+const form = document.getElementById('task-form');
+const taskList = document.getElementById('task-list');
+
+form.addEventListener('submit', async (e)=>{
+
+    e.preventDefault();
+
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('description').value
+})
