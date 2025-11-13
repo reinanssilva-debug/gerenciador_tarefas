9200c20d68b00984ad11004707dd27bd16774154
Reinan_aluno <reinan.s.silva@aluno.senai.br>
2025-11-13 14:56:38 -0300

    Criação funcões script.js

--- script.js
+++ script.js
@@ -3,10 +3,74 @@ const apiUrl= "http://localhosot;3000/tasks"
 const form = document.getElementById('task-form');
 const taskList = document.getElementById('task-list');
 
-form.addEventListener('submit', async (e)=>{
+form.addEventListener('submit', async (e)=> {
 
     e.preventDefault();
 
     const title = document.getElementById('title').value;
     const description = document.getElementById('description').value
-})
+
+try{
+    const res = await fetch(apiUrl,{
+     method: 'POST',
+     headers: { 'Content-Type': 'application/json'},
+     body: JSON.stringify({title,description})   
+    })
+
+    if(!res.ok) throw new Error("Erro ao adicionar tarefa");
+
+    const task = await res.json();
+    form.reset();
+    addTaskToUl(task);
+
+
+} catch (error){
+
+    alert("Erro ao salvar tarefa:"+ error.message);
+
+}
+
+
+});
+
+function addTaskToUl(task) {
+    const li = document.createElement("li");
+    li.className= task.completed ? "completed" : "";
+    li.innerHTML = `
+    <span>${task.title} - ${task.description}</span>
+    <div>
+        <button onClick="toggleCompleted(${task.id},${task.completed})">
+        ☑️
+        </button>
+        <button onClick="deleteTask(${task.id})">🗑️</button>
+    </div>
+    `;
+    taskList.apprendChild(li);
+
+}
+
+async function loadTasks() {
+    try{
+        const res = await fetch(apiUrl);
+        if(!res.ok) throw new Error("Erro ao carregar tarefas");
+   
+        const tasks = await res.json();
+        taskList.innerHTML = "";
+        tasks.forEach(addTaskToUl);
+    } catch (error) {
+        alert("Erro ao carregar tarefas:" + error.message);
+    }
+}
+
+async function toggleCompleted(id,completed){
+    try {
+        await fetch(`${apiUrl}/${id}`,{
+            method: "PUI",
+            headers: {"content-Type":"application/json"},
+            body: JSON.stringify({completed: !completed})
+        });
+    } catch (error) {
+        alert("erro ao atualizar tarefa:"+ error.message);
+    }
+
+}
