e03c67477d9dae88961a7ac6bf3afa6ef22e7b7b
Reinan_aluno <reinan.s.silva@aluno.senai.br>
2025-11-13 16:40:55 -0300

    backend criado

---
+++ backend/server..js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+//adicionar db.js
+
+const PORT = 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/tasks", (req,res) =>{
+    db.query("SELECT * FROM tasks",(err,result)=>{
+        if(err) return res.status(500).json({error: err.message});
+        res.json(result);
+    })
+}) 
